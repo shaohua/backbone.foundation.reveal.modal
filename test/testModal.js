ed7b3161380d3c9462f385abf593c30e0be1c0b4
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-25 20:17:35 -0700

    Basic test structure

--- test/testModal.js
+++ test/testModal.js
@@ -1,3 +1,15 @@
+/*global describe, it */
+
 var   chai = require('chai'),
     assert = chai.assert,
     expect = chai.expect;
+
+(function () {
+
+  describe('Basic assertions', function () {
+    it('should pass basic assertions', function () {
+      expect(true).to.equal(true);
+    });
+  });
+
+})();
