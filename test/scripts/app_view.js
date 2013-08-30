7054a936e5b20b7c06a6ec9b9f2293a0d5fc1900
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-30 16:34:13 -0700

    First two modals

--- test/scripts/app_view.js
+++ test/scripts/app_view.js
@@ -1,12 +1,37 @@
 TestModal.Views.AppView = Backbone.View.extend({
   el: $('#app'),
 
+  events: {
+    'click .first_modal': 'open_first_modal',
+    'click .second_modal': 'open_second_modal'
+  },
+
   initialize: function () {
     // body...
   },
 
+  open_first_modal: function(){
+    var modal = Backbone.FoundationModal({
+            title: "this is the modal title",
+            content: 'this is the modal content',
+            okText: 'OK'
+          });
+
+    modal.open();
+  },
+
+  open_second_modal: function(){
+    var modal = Backbone.FoundationModal({
+            title: "this is the modal title",
+            content: 'this is the modal content',
+            okText: 'OK'
+          });
+
+    modal.open();
+  },
+
   render: function(){
-    this.$el.html('test');
+    this.$el.find('.test').html('test');
     return this;
   }
 });
