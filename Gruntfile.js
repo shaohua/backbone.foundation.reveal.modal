8a16b810f26a47ef7c25f2daf66008f01347d401
Shaohua Zhou <sh.zhou@gmail.com>
2013-08-30 17:23:19 -0700

    Running basic test for now

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
         options: {
           reporter: 'spec'
         },
-        src: ['test/**/*.js']
+        src: ['test/testModal.js']
       }
     }
   });
